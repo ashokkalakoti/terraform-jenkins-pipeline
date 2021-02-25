2ec8333fd6d6622c21132a16cd73075c4f7f100a
Jyothi <jyothi@cloudreinforce.com>
2021-02-25 17:48:40 +0800

    adding changes from development machine

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-    source                  = "s3_module"
+    source                  = "./s3_module"
     s3_bucket_name          = "my-example-terraform-module-bucket2"
     versioning_enable       = "true"
     environment             = "UAT"
