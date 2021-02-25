b73a337558845f0e0ad5224c522ddf6e45091013
Jyothi <jyothi@cloudreinforce.com>
2021-02-25 17:07:26 +0800

    adding changes from development machine

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-example-terraform-module-bucket1"
-    key    = "projects/project-jenkins/terraform.tfstate"
+    key    = "projects/project-jenkins02/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
